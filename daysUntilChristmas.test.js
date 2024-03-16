afdb33ae5c26dbbf3b3c33e7e13ef992b6a6e9ae
Kevin Estrada <kevin.estradaloa@gmail.com>
2024-03-15 22:56:23 -0600

    Subida de nuevo archivo YAML de prueba unitaria examen-parcial-1 update a test

--- daysUntilChristmas.test.js
+++ daysUntilChristmas.test.js
@@ -1,6 +1,5 @@
 const daysUntilChristmas = require('./daysUntilChristmas');
 
 test('Days until Christmas', () => {
-  // Considering the current date is March 15, 2024
-  expect(daysUntilChristmas()).toBe(285); // Adjust accordingly for the current year
+  expect(daysUntilChristmas()).toBe(285); 
 });
--- dias_para_navida.js
+++ dias_para_navida.js
@@ -1,10 +1,10 @@
 function daysUntilChristmas() {
     const today = new Date();
-    const christmas = new Date(today.getFullYear(), 11, 25); // Month is 0-indexed (11 represents December)
+    const christmas = new Date(today.getFullYear(), 11, 25);
     if (today.getMonth() === 11 && today.getDate() > 25) {
         christmas.setFullYear(christmas.getFullYear() + 1);
     }
-    const oneDay = 1000 * 60 * 60 * 24; // Number of milliseconds in a day
+    const oneDay = 1000 * 60 * 60 * 24;
     return Math.ceil((christmas - today) / oneDay);
 }
 
